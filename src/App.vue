e33eefd62d4a1f9f81b9f9a8e359e276bea5e0e1
liuhui <1819364046@qq.com>
2018-03-14 21:11:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app" class="container">
+    <MyForm></MyForm>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyForm from './components/Form.vue'
+
+export default {
+  name: 'app',
+  data: {
+    first: 'aaa',
+    last: 'bbb'
+  },
+  methods: {
+    onSubmit: function (event) {
+      alert('submiting');
+    }
+  },
+  components: {
+    HelloWorld,
+    MyForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,29 @@
+<template>
+  <form method="post" action="/projects" @submit.prevent="onSubmit">
+    <div class="control">
+      <label for='first' class="label">Fist Name</label>
+      <input type="text" name="first" id="first" class="input" v-model="first" />
+      <span class="help is-danger"></span>
+    </div>
+    <div class="control">
+      <label for="last" class="label">Last Name</label>
+      <input type="text" name="last" id="last" class="input" v-model="last" />
+      <span class="help is-danger"></span>
+    </div>
+    <div class="control">
+      <button class="button is-primary">submit</button>
+    </div>
+  </form>
+</template>
+
+
+<script>
+export default {
+  name: 'MyForm'
+}
+</script>
+
+<style>
+  @import "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.css";
+</style>
+
